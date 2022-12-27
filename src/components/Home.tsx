4a42bad171f9204897e5e6f74cd82d80f31563c3
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-12-27 18:35:23 +0300

    Created the hero-section in the Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import Nav from './Nav';
 import Introduction from "./Introduction";
 import Footer from './Footer';
@@ -7,6 +8,36 @@ function Home() {
     return (
         <div className="w-full min-h-screen">
             <Nav />
+            <div className="w-full h-1/2 bg-neutral-900">
+                <div className="w-3/4 h-full ml-auto mr-auto flex">
+                    <div className="w-3/5 flex flex-col justify-center bg-neutral-900">
+                        <div className="w-1/2 mb-6">
+                            <h1 className="uppercase text-lg text-neutral-500 tracking-widest">
+                                new product
+                            </h1>
+                        </div>
+                        <div className="w-3/5 flex flex-col space-y-6">
+                            <h1 className="uppercase text-6xl text-white font-bold">
+                                xx99 mark ii headphones
+                            </h1>
+                            <p className="text-gray-400">
+                                Experience natural, lifelike audio and exceptional build quality made for the passionate music
+                                enthusiast.
+                            </p>
+                            <Link className="w-1/4 h-10 bg-orange-500 text-white text-md flex justify-center items-center uppercase hover:text-gray-400" to={""}>
+                                see product
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="w-2/5 h-full flex justify-center">
+                        <img
+                            className="w-full h-1/3"
+                            src="../assets/actual-hero-image.png"
+                            alt="hero"
+                        />
+                    </div>
+                </div>
+            </div>
             <Introduction />
             <Footer />
         </div>
