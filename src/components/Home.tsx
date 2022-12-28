34bd3fa1b38c7df3047380ecbd26b5c4c878c782
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-12-28 04:38:07 +0300

    Added product preview section in the home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -25,7 +25,7 @@ function Home() {
                                 Experience natural, lifelike audio and exceptional build quality made for the passionate music
                                 enthusiast.
                             </p>
-                            <Link className="w-1/4 h-10 bg-orange-500 text-white text-md flex justify-center items-center uppercase hover:text-gray-400" to={""}>
+                            <Link className="w-1/4 h-10 bg-orange-500 text-white text-sm flex justify-center items-center uppercase hover:text-gray-400" to={""}>
                                 see product
                             </Link>
                         </div>
@@ -40,6 +40,56 @@ function Home() {
                 </div>
             </div>
             <ProductThumbnail />
+            <div className="w-3/4 bg-orange-500 ml-auto mr-auto rounded-md mt-48 flex bg-pattern bg-no-repeat">
+                <div className="w-3/5 h-66 flex justify-center">
+                    <img
+                        className="w-1/2 h-full mt-2"
+                        src="../assets/image-speaker-zx9.png"
+                        alt="speaker"
+                    />
+                </div>
+                <div className="w-1/4 flex flex-col justify-center space-y-6">
+                    <h1 className="text-white uppercase font-bold text-6xl">
+                        zx9 speaker
+                    </h1>
+                    <p className="text-gray-300">
+                        Upgrade to premium speakers that are phenomenally built to deliver
+                        truly remarkable sound.
+                    </p>
+                    <Link className="w-2/5 h-10 flex items-center text-sm justify-center text-white bg-black uppercase hover:text-gray-400" to={""}>
+                        see product
+                    </Link>
+                </div>
+            </div>
+            <div className="w-3/4 h-80 ml-auto mr-auto bg-background bg-cover rounded-md mt-12">
+                <div className="w-3/4 h-full ml-auto mr-auto flex flex-col justify-center space-y-6">
+                    <h1 className="uppercase text-3xl font-bold">
+                        zx7 speaker
+                    </h1>
+                    <Link className="w-1/6 h-10 flex items-center text-sm justify-center font-bold border border-black uppercase hover:text-gray-400" to={""}>
+                        see product
+                    </Link>
+                </div>
+            </div>
+            <div className="w-3/4 h-78 ml-auto mr-auto mt-12 flex space-x-8">
+                <div className="w-1/2 rounded-md">
+                    <img
+                        className="w-full h-full rounded-md"
+                        src="../assets/image-earphones-yx1.jpg"
+                        alt="earphones"
+                    />
+                </div>
+                <div className="w-1/2 bg-gray-200 rounded-md">
+                    <div className="w-3/5 h-full ml-auto mr-auto flex flex-col justify-center space-y-6">
+                        <h1 className="uppercase text-4xl font-bold">
+                            yx1 earphones
+                        </h1>
+                        <Link className="w-1/3 h-12 flex items-center text-sm justify-center font-bold border border-black uppercase hover:text-gray-400" to={""}>
+                            see product
+                        </Link>
+                    </div>
+                </div>
+            </div>
             <Introduction />
             <Footer />
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
         'sans' : ['Manrope', ...defaultTheme.fontFamily.sans],
       }
     },
+    backgroundImage: {
+      'pattern': "url('../public/assets/pattern-circles.svg')",
+      'background': "url('../public/assets/image-speaker-zx7.jpg')",
+    }
   },
   plugins: [],
 }
