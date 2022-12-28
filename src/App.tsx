35a01c5639dcf85d55977259dd8ee72482730be4
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-12-29 00:37:13 +0300

    Created category earphones page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Headphones from "./pages/Headphones";
 import Speakers from "./pages/Speakers";
+import Earphones from "./pages/Earphones";
 import data from './data.json';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
                 <Route index element={<Home />} />
                 <Route path="headphones" element={<Headphones products={products} />} />
                 <Route path="speakers" element={<Speakers products={products} />} />
+                <Route path="earphones" element={<Earphones products={products} />} />
             </Routes>
         </BrowserRouter>
   );
